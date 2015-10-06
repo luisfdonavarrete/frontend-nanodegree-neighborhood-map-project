9f83dba85e88e2469a96e685eb96c78af4c261be
Luis Navarrete <luis.na06@gmail.com>
2015-10-06 01:30:08 -0400

    Add infowindow, resolve arrow bug :)

--- assets/js/app.js
+++ assets/js/app.js
@@ -507,14 +507,18 @@ a+" })()) }}"};this.addTemplate=function(a,b){w.write("<script type='text/html' 
 	    map,
 	    infowindow,
 	    bounds,
-	    previousMarker;
+	    previousMarker,
+	    infoWindowTemplate;
 
 	function Location(data) {
 		var self = this;
 		this.name = ko.observable(data.name);
 		this.latitude = ko.observable(data.location.lat);
 		this.longitude = ko.observable(data.location.lng);
-		this.location = ko.observable(data.location);
+		this.address = ko.observable(data.location.address);
+		this.phoneNumber = ko.observable(data.contact.formattedPhone);
+		this.webSite = ko.observable(data.url);
+		this.link = ko.observable(encodeURIComponent("http://localhost:8000/" + data.name));
 		this.marker = ko.observable(
 			new google.maps.Marker({			
 				position: new google.maps.LatLng(data.location.lat, data.location.lng),
@@ -532,7 +536,11 @@ a+" })()) }}"};this.addTemplate=function(a,b){w.write("<script type='text/html' 
 				previousMarker = undefined;
 			}
 			else{
-				infowindow.setContent(data.name);
+				infowindow.setContent(infoWindowTemplate({
+					name : self.name(),
+					address : self.address(),
+					phoneNumber : self.phoneNumber()
+				}));
 				self.marker().setAnimation(google.maps.Animation.BOUNCE);
 				previousMarker && previousMarker.setAnimation(null);
 				infowindow.open(map, self.marker());
@@ -633,16 +641,66 @@ a+" })()) }}"};this.addTemplate=function(a,b){w.write("<script type='text/html' 
 	return function (){
 		bounds = new google.maps.LatLngBounds();
 		map = new google.maps.Map(document.getElementById('map-section'), {
-			center: {lat: 43.472975, lng: -79.687325},
 			zoom: 12
 		});
 		infowindow = new google.maps.InfoWindow();
+		/**/
+		google.maps.event.addListener(infowindow, 'domready', function() {
+
+			// Reference to the DIV that wraps the bottom of infowindow
+			var iwOuter = $('.gm-style-iw');
+
+			/* Since this div is in a position prior to .gm-div style-iw.
+     * We use jQuery and create a iwBackground variable,
+     * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
+    */
+			var iwBackground = iwOuter.prev();
+
+			// Removes background shadow DIV
+			iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+			// Removes white background DIV
+			iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+			// Moves the infowindow 115px to the right.
+			iwOuter.parent().parent().css({left: '115px'});
+
+			// Moves the shadow of the arrow 76px to the left margin.
+			iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+			// Moves the arrow 76px to the left margin.
+			var moveArrow = function(){
+				iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+				window.requestAnimationFrame(moveArrow);	
+			};
+			window.requestAnimationFrame(moveArrow);			
+
+			// Changes the desired tail shadow color.
+			iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+
+			// Reference to the div that groups the close button elements.
+			var iwCloseBtn = iwOuter.next();
+
+			// Apply the desired effect to the close button
+			iwCloseBtn.css({opacity: '1', right: '38px', top: '3px', border: '7px solid #48b5e9', 'border-radius': '13px', 'box-shadow': '0 0 5px #3990B9'});
+
+			// If the content of infowindow not exceed the set maximum height, then the gradient is removed.
+			if($('.iw-content').height() < 140){
+				$('.iw-bottom-gradient').css({display: 'none'});
+			}
+
+			// The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
+			iwCloseBtn.mouseout(function(){
+				$(this).css({opacity: '1'});
+			});
+		});
+		/**/
 		google.maps.event.addDomListener(window, "resize", function() {
 			var center = map.getCenter();
 			google.maps.event.trigger(map, "resize"); 
 			map.setCenter(center); 
 		});
-		
+		infoWindowTemplate = _.template($("#info-window-template").html());		
 		$(".box-shadow-menu").click(function(e) {
 			e.preventDefault(); 
 			$("#wrapper").toggleClass("toggled");
