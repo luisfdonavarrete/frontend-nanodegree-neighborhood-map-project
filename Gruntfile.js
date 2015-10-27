4efcc36fe6143509b0c5477690980eb3a52f7723
Luis Navarrete <luis.na06@gmail.com>
2015-10-27 16:47:39 -0400

    Minify Js and css only if we are in a prodiction enviroment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,82 +1,86 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON("package.json"),
+		dirs: {
+			output : "assets"			
+		},
+		env: grunt.option('env') || process.env.GRUNT_ENV || 'development',
 		clean: {
 			options: {
 				force: true
 			},
-			js:    ["assets/js/"],
+			js:    ["<%= dirs.output %>/js/"],
 			css:   [
-				"assets/css/styles.min.css"
+				"<%= dirs.output %>/css/styles.min.css"
 			]
 		},
 		concat: {
 			options: {
-				separator: ';'
+				separator: ";"
 			},
 			script: {
 				src: [
-					'bower_components/knockout/dist/knockout.js',
-					'develop/js/app.js'
+					"bower_components/jquery/dist/jquery.js",
+					"bower_components/underscore/underscore.js",
+					"bower_components/jquery-ui/jquery-ui.js",
+					"bower_components/knockout/dist/knockout.js",
+					"develop/js/app.js"
 				],
-				dest: 'assets/js/app.js'
+				dest: "<%= dirs.output %>/js/app.min.js"
 			}
 		},
 		uglify: {
-			dist: {
-				files: {
-					'assets/js/app.min.js': [
-						'bower_components/jquery/dist/jquery.min.js',
-						'bower_components/underscore/underscore-min.js',
-						'bower_components/jquery-ui/jquery-ui.min.js',
-						'assets/js/app.js']
+				dist: {
+					files: {
+						"<%= dirs.output %>/js/app.min.js": ["<%= dirs.output %>/js/app.min.js"]
+					}
 				}
+		},
+		concat_css: {
+			files: {
+				src: [
+					"bower_components/bootstrap/dist/css/bootstrap.css",
+					"develop/css/styles.css"	
+				],
+				dest: "<%= dirs.output %>/css/styles.min.css"
 			}
 		},
 		cssmin: {
-			options: {
-				shorthandCompacting: false,
-				roundingPrecision: -1
-			},
 			target: {
 				files: {
-					'assets/css/styles.min.css': [
-						'bower_components/bootstrap/dist/css/bootstrap.css',
-						'develop/css/styles.css'
-					]
+					"<%= dirs.output %>/css/styles.min.css": ["<%= dirs.output %>/css/styles.min.css"]
 				}
 			}			
 		},
 		critical: {
 			test: {
 				options: {
-					base: '../',
 					css: [
-						'assets/css/styles.min.css'
+						"<%= dirs.output %>/css/styles.min.css"
 					],
 					width: 320,
 					height: 70
 				},
-				src: 'develop/index.html',
-				dest: 'index.html'
+				src: "develop/index.html",
+				dest: "index.html"
 			}
 		},
 		watch: {
 			grunt: { 
-				files: ['Gruntfile.js'], 
-				tasks: ['default'] 
+				files: ["Gruntfile.js"], 
+				tasks: ["default"] 
 			},
 			scripts: {
-				files: ['develop/js/**/*.js'],
-				tasks: ['jsTask'],
+				files: ["develop/js/**/*.js"],
+				tasks: ["jsTask"],
 				options: {
 					spawn: false
 				}
 			},
 			css: {
-				files: ['develop/css/**/*.css', 'develop/index.html'],
-				tasks: ['cssTask'],
+				files: ["develop/css/**/*.css", "develop/index.html"],
+				tasks: ["cssTask"],
 				options: {
 					spawn: false
 				},
@@ -84,16 +88,36 @@ module.exports = function(grunt) {
 		},
 
 	});
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-critical');
+	
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-concat-css");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-critical");
+	grunt.loadNpmTasks("grunt-env");
+	
 	// Default task(s).
+	
+	grunt.registerTask("cssTask", (function(){
+		if(grunt.config('env') === "development"){
+			return ["clean:css", "concat_css", "critical"];
+		}
+		else{
+			return ["clean:css", "concat_css", "cssmin", "critical"];
+		}	
+	})());
 
-	grunt.registerTask('cssTask', ['clean:css', 'cssmin', 'critical']);
-	grunt.registerTask('jsTask',  ['clean:js', 'concat', 'uglify']);
-	grunt.registerTask('default',  ['clean', 'cssTask', 'jsTask','watch']);
+	grunt.registerTask("jsTask", (function(){
+		if(grunt.config('env') === "development"){
+			return ["clean:js", "concat"];
+		}
+		else{
+			return ["clean:js", "concat", "uglify"];
+		}	
+	})());
+	
+	grunt.registerTask("default",  ["clean", "cssTask", "jsTask","watch"]);
 
 };
